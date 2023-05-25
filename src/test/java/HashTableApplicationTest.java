cf7a631ca20da60e0ba7794c387a27ee52ceebc2
autida <autidaanalita@gmail.com>
2023-05-25 16:45:41 +0800

    created test for hash table application

--- src/test/java/HashTableApplicationTest.java
+++ src/test/java/HashTableApplicationTest.java
@@ -1,2 +1,15 @@
+import org.example.HashTableApplications;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HashTableApplicationTest {
+    @Test
+    public void methodTest1_success() {
+        Assertions.assertEquals('c', HashTableApplications.method1("abacabad"));
+    }
+
+    @Test
+    public void methodTest2_no_nonRepeatingCharacter() {
+        Assertions.assertEquals('_', HashTableApplications.method1("abaaabab"));
+    }
 }
