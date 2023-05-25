6e06f5ca4161b3597d6112d275599a99fd91976a
autida <autidaanalita@gmail.com>
2023-05-25 16:40:49 +0800

    implemented hashTableApplication

--- src/main/java/org/example/HashTableApplications.java
+++ src/main/java/org/example/HashTableApplications.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class HashTableApplications {
+    public static char method1(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()) {
+            if(map.containsKey(c)) {
+                int count = map.get(c) + 1;
+                map.put(c, count);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for(char c: map.keySet()) {
+            if(map.get(c) == 1) {
+                return c;
+            }
+        }
+        return '_';
+    }
 }
